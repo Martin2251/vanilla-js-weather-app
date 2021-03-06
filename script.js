bccee1eec60cd525b4af492a2ec0fc2dee91e536
martin lowe <martinlowe673@gmail.com>
2021-03-06 11:37:05 +0000

    added const

---
+++ script.js
@@ -0,0 +1,7 @@
+const date = document.getElementById("date");
+const temperature = document.getElementById("temp");
+const city = document.getElementById("location");
+const weather = document.getElementById("weather");
+const searchBar = document.getElementById("search-bar");
+
+//the function i need is to get weather
